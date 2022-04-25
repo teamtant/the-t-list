e367be9c4422f0697447f5cff4236193003d8240
tbarry16 <tim.barry12@gmail.com>
2022-04-25 13:08:39 -0400

    Review page working

--- client/components/CreateReview.jsx
+++ client/components/CreateReview.jsx
@@ -1,14 +1,16 @@
 import React, {useState} from 'react'
 
 const CreateReview = ({ coords }) => {
+
+    const handleClick = () => {
+        console.log('hello')
+    }
     console.log(coords)
   return (
     <div className='review-form'>
         <h1>Create Review</h1>
-        <label>Provider:</label>
-        <input type='text' placeholder='Enter provider'></input>
-        <label>Provider Location:</label>
-        <input type='text' placeholder='Enter location'></input>
+        <label>Clinic:</label>
+        <input type='text' placeholder='Enter clinic'></input>
         <label>Type of Service:</label>
         <input type='text' placeholder='Enter type of service'></input>
         <label>Cost:</label>
@@ -28,17 +30,9 @@ const CreateReview = ({ coords }) => {
         </select>        
         <label>Review:</label>
         <textarea rows='15' placeholder='Enter a review' ></textarea>
+        <button onClick={handleClick}>Submit Review</button>
     </div>
   )
 }
 
 export default CreateReview
-
-
-
-// fetch api (need to get this endpoint from backend)
-// POST method, headers etc
-// body obj {provider, coordinates, type of service, cost, rating, review description}
-// send as json
-
-// some conditional functionality to either post to existing location or post new location
