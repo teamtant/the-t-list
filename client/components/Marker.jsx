088bff884957e9958814aa277300335798eb3ed1
tbarry16 <tim.barry12@gmail.com>
2022-04-25 17:06:53 -0400

    testing

--- client/components/Marker.jsx
+++ client/components/Marker.jsx
@@ -3,6 +3,7 @@ import { InfoWindow } from 'google-maps-react';
 
 const Marker = (options) => {
 	const [marker, setMarker] = React.useState();
+	const [reviews, setReviews] = React.useState();
 
 
 	React.useEffect(() => {
@@ -23,43 +24,46 @@ const Marker = (options) => {
 
 			marker.addListener('click', (options) => {
 				marker.changeCoords([marker.position.lat(), marker.position.lng()], marker.id)
-				
+
 				fetch('/api/' + marker.id)
 				  .then(response => response.json())
-				  .then(data => console.log(data))
+				  .then(data => {
 
-				const starObj = {
-					1: '✩',
-					2: '✩✩',
-					3: '✩✩✩',
-					4: '✩✩✩✩',
-					5: '✩✩✩✩✩'
-				}
-
-				const contentString = 
-				`<div>
-					<h1>` + marker.clinicName + `</h1>
-					<h3>Review 1</h3>
-					<ul>
-						
-						<li><strong>Rating:</strong>` + starObj[3] + ` /*reviews[i].rating*/</li>
-						<li><strong>Service Type:</strong> service /*reviews[i].rating*/</li>
-						<li><strong>Cost:</strong> cost /*reviews[i].rating*/</li>
-						<li><strong>Review:</strong> review /*reviews[i].rating*/</li>
-					</ul>
-				</div>`
-
-				console.log('we are logging the marker clinic ' + marker.clinicName)
-				const infoWindow = new google.maps.InfoWindow(
-					{content: contentString}
-				);
-
-
-				infoWindow.open({
-					anchor: marker,
-					map: options.map,
-					shouldFocus: true,
-				});
+					  const starObj = {
+						  1: '✩',
+						  2: '✩✩',
+						  3: '✩✩✩',
+						  4: '✩✩✩✩',
+						  5: '✩✩✩✩✩'
+					  }
+	  
+					  const contentString = 
+					  `<div>
+						  <h1>` + marker.clinicName + `</h1>
+						  <h3>1</h3>
+						  <ul>
+							  
+							  <li><strong>Rating:</strong>` + starObj[3] + ` /*reviews[i].rating*/</li>
+							  <li><strong>Service Type:</strong> service /*reviews[i].rating*/</li>
+							  <li><strong>Cost:</strong> cost /*reviews[i].rating*/</li>
+							  <li><strong>Review:</strong> review /*reviews[i].rating*/</li>
+						  </ul>
+					  </div>`
+	  
+					  console.log('we are logging the marker clinic ' + marker.clinicName)
+					  const infoWindow = new google.maps.InfoWindow(
+						  {content: contentString}
+					  );
+	  
+	  
+					  infoWindow.open({
+						  anchor: marker,
+						  map: options.map,
+						  shouldFocus: true,
+					  });
+				  })
+				  
+					
 			});
 		}
 	}, [marker, options]);
